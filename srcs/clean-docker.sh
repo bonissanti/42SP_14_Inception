aca62cc48eacec29136294070410da19b9d434de
Bruno Onissanti <brunoonissanti@gmail.com>
2024-10-22 18:52:15 -0300

    fix: wordpress database bug

--- srcs/clean-docker.sh
+++ srcs/clean-docker.sh
@@ -5,3 +5,5 @@ docker stop $(docker container ls -aq)
 
 # Remove all containers, images, networks, and volumes
 docker system prune -af --volumes
+docker volume rm $(docker volume ls)
+docker network rm $(docker network ls)
--- srcs/requirements/wordpress/tools/run.sh
+++ srcs/requirements/wordpress/tools/run.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-# mkdir -p /var/www/html/
-# chown -R www-data: /var/www/html
-
-# wget https://wordpress.org/latest.tar.gz
-# tar -xzvf latest.tar.gz
-# rm latest.tar.gz
-
-# rm -rf /var/www/html/wordpress
-# mv wordpress /var/www/html/
-
 #installing wp-cli
 wget -O wp-cli.phar https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
@@ -19,12 +9,16 @@ mv wp-cli.phar /usr/local/bin/wp
 wp core download --allow-root --path=/var/www/html
 
 #generating config file
+rm -rf wordpress
 wp --allow-root config create --dbname="$WORDPRESS_DATABASE" --dbuser="$WORDPRESS_USER" \
     --dbpass="$WORDPRESS_PASSWORD" --dbhost="mariadb"
 
 #installing wordpress
 wp --allow-root core install --url=brunrodr.42.fr --title="Inception" --admin_user=$WORDPRESS_USER \
     --admin_password=$WORDPRESS_PASSWORD --admin_email=lalala@email.com
+
+# Second user (non admin)
+wp --allow-root user create second_user second_user@email.com --role=editor --user_pass=$WORDPRESS_SECOND_USER_PWD
 
 exec php-fpm7.4 -F
 
